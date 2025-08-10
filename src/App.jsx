036c12fcd1fd44091cd59a854eb439a11edf38e1
Matheus Pontes <matheuspontes5@hotmail.com>
2025-08-10 19:12:37 -0300

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.scss';
+import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
-    <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove commented lines :) */}
+    <Sum n1={2} n2={3} result={5}/>
+    <Sum n1={-5} n2={5} result={0}/>
+    <Sum n1={10} n2={0} result={10}/>
+    <Sum n1={0} n2={5} result={5}/>
+    <Sum n1={0} n2={0} result={0}/>
   </>
 );
--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,4 @@
-// export const Sum = () => ();
+
+export const Sum = ({ n1, n2, result }) => (
+    <p>Sum of {n1} and {n2} is {result}</p>
+)
